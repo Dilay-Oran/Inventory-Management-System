3e21af7f4585e093a371b5ce21a9ce49e24a2469
DilayOran <dilay.oran@std.bogazici.edu.tr>
2026-01-02 19:28:25 +0300

    sql lite connection added and tables created

---
+++ Models/Duties.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace Inventory_Management_System.Models
+{
+   public class DutiesItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty; // not nullable and default value is empty
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
