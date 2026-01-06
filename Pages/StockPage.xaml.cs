e699f226f16c4cf65380ae0105b5c4519eb3d548
DilayOran <dilay.oran@std.bogazici.edu.tr>
2026-01-06 23:09:23 +0300

    StockPage.xaml strated to be created

--- Pages/StockPage.xaml.cs
+++ Pages/StockPage.xaml.cs
@@ -1,3 +1,5 @@
+using Inventory_Management_System.Data;
+
 namespace Inventory_Management_System.Pages;
 
 public partial class StockPage : ContentPage
@@ -6,4 +8,17 @@ public partial class StockPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private ProductsDB productsdb = new ProductsDB();
+    private WarehouseDB warehousedb = new WarehouseDB();
+    private StockDB stockdb = new StockDB();
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+    private async Task LoadProducts()
+    {
+        var products = await productsdb.GetAllAsync(); // productdbden ürünleri çekecek
+        ProductPicker.ItemsSource = products;// pickerýn içine atacak
+    }
 }
